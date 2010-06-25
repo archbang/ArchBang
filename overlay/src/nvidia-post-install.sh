33e2fe94b106595fbfa9ee4904521ba3cf709dde
pdg <pdg@archlinux.us>
2010-06-25 10:25:06 +0530

    upgrade nvidia packages to 256

--- overlay/src/nvidia-post-install.sh
+++ overlay/src/nvidia-post-install.sh
@@ -16,7 +16,7 @@ echo
 	pacman -Rsnd xorg-video-drivers -r ${DESTDIR} --noconfirm --noprogressbar &>/dev/null
 	pacmn -Rsnd libgl -r ${DESTDIR} --noconfirm --noprogressbar &>/dev/null
         pacman -Ufd /src/pkgs/nvidia-utils* -r ${DESTDIR} --noconfirm --noprogressbar &>/dev/null
-        pacman -Ufd /src/pkgs/nvidia-1* -r ${DESTDIR} --noconfirm --noprogressbar &>/dev/null
+        pacman -Ufd /src/pkgs/nvidia-2* -r ${DESTDIR} --noconfirm --noprogressbar &>/dev/null
 	rm -f ${DESTDIR}/etc/X11/xorg.conf  &>/dev/null
 	cp /etc/X11/xorg.conf ${DESTDIR}/etc/X11/xorg.conf  &>/dev/null
 echo
