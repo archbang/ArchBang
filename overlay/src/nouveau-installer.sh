7cd4cc9f5dff370f4bdde8d9345f3949471cb026
pdg <pdg@archlinux.us>
2010-06-29 21:00:15 +0530

    trying to fix nouveau

--- overlay/src/nouveau-installer.sh
+++ overlay/src/nouveau-installer.sh
@@ -5,6 +5,6 @@ pacman -Ufd /src/pkgs/nouveau-dri-* --noprogressbar --noconfirm &> /dev/null
 pacman -Ufd /src/pkgs/nouveau-firmware-* --noprogressbar --noconfirm &> /dev/null
 pacman -Ufd /src/pkgs/xf86-video-nouveau-* --noprogressbar --noconfirm &> /dev/null
 modprobe nouveau 
-X -configure &> /dev/null
-cp /root/xorg.conf.new /etc/X11/xorg.conf &> /dev/null	
+#X -configure &> /dev/null
+#cp /root/xorg.conf.new /etc/X11/xorg.conf &> /dev/null	
 
