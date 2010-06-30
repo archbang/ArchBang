7c6d1a6c2855123cb27fad19da9a969df06eef29
pdg <pdg@archlinux.us>
2010-06-30 11:07:30 +0530

    keeping nouevau installed instead of on-the-fly install

--- overlay/src/nouveau-post-installer.sh
+++ overlay/src/nouveau-post-installer.sh
@@ -6,9 +6,9 @@ echo "Nouveau Opensource NVIDIA driver is being installed on your system . . ."
 echo
 echo "Read more about it here: http://wiki.archlinux.org/index.php/Nouveau"
 pacman -Rsnd xorg-video-drivers -r ${DESTDIR} --noconfirm --noprogressbar &>/dev/null
-pacman -Ufd /src/pkgs/nouveau-dri-* -r $DESTDIR --noprogressbar --noconfirm &> /dev/null
-pacman -Ufd /src/pkgs/nouveau-firmware-* -r $DESTDIR --noprogressbar --noconfirm &> /dev/null
-pacman -Ufd /src/pkgs/xf86-video-nouveau-* -r $DESTDIR --noprogressbar --noconfirm &> /dev/null
+#pacman -Ufd /src/pkgs/nouveau-dri-* -r $DESTDIR --noprogressbar --noconfirm &> /dev/null
+#pacman -Ufd /src/pkgs/nouveau-firmware-* -r $DESTDIR --noprogressbar --noconfirm &> /dev/null
+#pacman -Ufd /src/pkgs/xf86-video-nouveau-* -r $DESTDIR --noprogressbar --noconfirm &> /dev/null
 cp /etc/X11/xorg.conf $DESTDIR/etc/X11/xorg.conf &> /dev/null	
 cp /src/kms/mkinitcpio.conf.nouveau $DESTDIR/etc/mkinitcpio.conf
 echo "Done."
